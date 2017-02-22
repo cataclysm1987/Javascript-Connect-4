6d3759b8e007915123e167dfbf31738a131d4e7a
cataclysm1987 <conklin.eric@gmail.com>
2017-02-22 16:24:21 -0500

    Add place scripts. Update style.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -47,3 +47,120 @@ function DropRow6() {
 function UpdateColor() {
     
 }
+
+//Place methods for player 1
+
+//Row 0
+function Player1Place00() {
+    $("#00").append('<div class="red-circle"></div>');
+    Board[0][0] = 1;
+}
+
+function Player1Place01() {
+    $("#01").append('<div class="red-circle"></div>');
+    Board[0][1] = 1;
+}
+
+function Player1Place02() {
+    $("#02").append('<div class="red-circle"></div>');
+    Board[0][3] = 1;
+}
+
+function Player1Place03() {
+    $("#03").append('<div class="red-circle"></div>');
+    Board[0][3] = 1;
+}
+
+function Player1Place04() {
+    $("#04").append('<div class="red-circle"></div>');
+    Board[0][4] = 1;
+}
+
+function Player1Place05() {
+    $("#05").append('<div class="red-circle"></div>');
+    Board[0][5] = 1;
+}
+
+function Player1Place06() {
+    $("#06").append('<div class="red-circle"></div>');
+    Board[0][6] = 1;
+}
+
+//Row 1
+function Player1Place10() {
+    $("#10").append('<div class="red-circle"></div>');
+    Board[1][0] = 1;
+}
+
+function Player1Place11() {
+    $("#11").append('<div class="red-circle"></div>');
+    Board[1][1] = 1;
+}
+
+function Player1Place12() {
+    $("#12").append('<div class="red-circle"></div>');
+    Board[1][2] = 1;
+}
+
+function Player1Place13() {
+    $("#13").append('<div class="red-circle"></div>');
+    Board[1][3] = 1;
+}
+
+function Player1Place14() {
+    $("#14").append('<div class="red-circle"></div>');
+    Board[1][4] = 1;
+}
+
+function Player1Place15() {
+    $("#15").append('<div class="red-circle"></div>');
+    Board[1][5] = 1;
+}
+
+function Player1Place16() {
+    $("#16").append('<div class="red-circle"></div>');
+    Board[1][6] = 1;
+}
+
+//Row 2
+
+function Player1Place20() {
+    $("#20").append('<div class="red-circle"></div>');
+    Board[2][0] = 1;
+}
+
+function Player1Place21() {
+    $("#21").append('<div class="red-circle"></div>');
+    Board[2][1] = 1;
+}
+
+function Player1Place22() {
+    $("#22").append('<div class="red-circle"></div>');
+    Board[2][2] = 1;
+}
+
+function Player1Place23() {
+    $("#23").append('<div class="red-circle"></div>');
+    Board[2][3] = 1;
+}
+
+function Player1Place24() {
+    $("#24").append('<div class="red-circle"></div>');
+    Board[2][4] = 1;
+}
+
+function Player1Place25() {
+    $("#25").append('<div class="red-circle"></div>');
+    Board[2][5] = 1;
+}
+
+function Player1Place26() {
+    $("#26").append('<div class="red-circle"></div>');
+    Board[2][6] = 1;
+}
+
+//Row 3
+
+//Row 4
+
+//Row 5
