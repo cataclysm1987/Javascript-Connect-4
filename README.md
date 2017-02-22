e632617704ec2211c2e8068168ef2cdb19b5446b
cataclysm1987 <conklin.eric@gmail.com>
2017-02-22 09:29:15 -0500

    update script and readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Javascript Connect 4
+Play Connect 4 with a partner easily with this Javascript application! Designed for 2 players sitting at the same computer.
---
+++ scripts/scripts.js
@@ -0,0 +1,49 @@
+﻿//Initial Values
+
+var CurrentPlayerTurn = 1;
+var Board = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0]
+];
+console.log(items[0][0]); // 1
+console.log(items);
+
+function CheckWin() {
+    
+}
+
+function DropRow0() {
+    
+}
+
+function DropRow1() {
+
+}
+
+function DropRow2() {
+
+}
+
+function DropRow3() {
+
+}
+
+function DropRow4() {
+
+}
+
+function DropRow5() {
+
+}
+
+function DropRow6() {
+
+}
+
+function UpdateColor() {
+    
+}
